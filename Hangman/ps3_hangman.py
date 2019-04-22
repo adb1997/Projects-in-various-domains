d244e4b9ded21668192f533b1a3107c9467bd0a7
Aditya Bhawkar <adityabhawkar@gmail.com>
2019-04-22 11:32:36 +0530

    Update ps3_hangman.py

--- Hangman/ps3_hangman.py
+++ Hangman/ps3_hangman.py
@@ -1,11 +1,4 @@
-# Hangman game
-#
 
-# -----------------------------------
-# Helper code
-# You don't need to understand this helper code,
-# but you will have to know how to use the functions
-# (so be sure to read the docstrings!)
 
 import random
 
@@ -14,9 +7,7 @@ WORDLIST_FILENAME = "words.txt"
 def loadWords():
     """
     Returns a list of valid words. Words are strings of lowercase letters.
-    
-    Depending on the size of the word list, this function may
-    take a while to finish.
+ 
     """
     print("Loading word list from file...")
     # inFile: file
@@ -36,11 +27,7 @@ def chooseWord(wordlist):
     """
     return random.choice(wordlist)
 
-# end of helper code
-# -----------------------------------
 
-# Load the list of words into the variable wordlist
-# so that it can be accessed from anywhere in the program
 wordlist = loadWords()
 
 def isWordGuessed(secretWord, lettersGuessed):
@@ -94,26 +81,6 @@ def getAvailableLetters(lettersGuessed):
     
 
 def hangman(secretWord):
-    '''
-    secretWord: string, the secret word to guess.
-
-    Starts up an interactive game of Hangman.
-
-    * At the start of the game, let the user know how many 
-      letters the secretWord contains.
-
-    * Ask the user to supply one guess (i.e. letter) per round.
-
-    * The user should receive feedback immediately after each guess 
-      about whether their guess appears in the computers word.
-
-    * After each round, you should also display to the user the 
-      partially guessed word so far, as well as letters that the 
-      user has not yet guessed.
-
-    Follows the other limitations detailed in the problem write-up.
-    '''
-    # FILL IN YOUR CODE HERE...
 
     print('Welcome to the game, Hangman!')
     print("I am thinking of a word that is", len(secretWord), "letters long")
@@ -162,8 +129,4 @@ hangman(secretWord)
 
 
 
-
-# When you've completed your hangman function, uncomment these two lines
-# and run this file to test! (hint: you might want to pick your own
-# secretWord while you're testing)
 
